47c136dc0d3c869ad09d8477387ea35443ac4cef
MohamedElmdary <engm5081@gmail.com>
2021-11-07 12:53:00 +0200

    tested kubernetes

---
+++ src/views/Editor.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    editor works!
+
+    <tf-kubernetes></tf-kubernetes>
+
+    <script
+      type="application/javascript"
+      src="/build/elements/kubernetes.wc.js"
+      defer
+    ></script>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  name: "Editor",
+})
+export default class Editor extends Vue {}
+</script>
