026cf932e56917dbd53a0d0b2c24465c457ee380
MohamedElmdary <engm5081@gmail.com>
2021-11-08 17:00:15 +0200

    updated build to fix js load issue

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -6,14 +6,6 @@
       </div>
       <div class="editor__preview" v-html="html"></div>
     </div>
-
-    <script
-      v-for="el in elements"
-      :key="el"
-      type="application/javascript"
-      :src="`/build/elements/${el}.wc.js`"
-      defer
-    ></script>
   </div>
 </template>
 
@@ -43,10 +35,6 @@ export default class Editor extends Vue {
   
   `;
   html = "";
-
-  created() {
-    console.log(this.elements);
-  }
 
   @Watch("md", { immediate: true, deep: true })
   onMdChange() {
